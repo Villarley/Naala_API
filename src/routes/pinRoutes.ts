8769b58f63ff565d539d59a627fdd9132b84f9f9
villarley <santivillarley1010@gmail.com>
2025-01-21 11:23:36 -0600

    feat: api working

--- src/routes/pinRoutes.ts
+++ src/routes/pinRoutes.ts
@@ -3,7 +3,7 @@ import { generatePin, verifyPin } from '../controllers/pinController';
 
 const router = express.Router();
 
-
+router.post("/generate", generatePin as any)
 router.post('/verifyPin', verifyPin as any);
 
 export default router;
