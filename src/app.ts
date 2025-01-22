5fff02aba708c3b3cc825b9a839c0d732f3796b5
villarley <santivillarley1010@gmail.com>
2025-01-22 17:20:40 -0600

    test

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,11 @@ import cors from 'cors';
 dotenv.config();
 connectDB();
 
-const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-};
 
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/",
     (req, res)=>{
         res.json({msg:"saludos"})
