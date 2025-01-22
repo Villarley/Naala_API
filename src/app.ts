b6102d1ba2cfe22c2fa676c64d97f945cc829f25
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-22 13:14:38 -0600

    deploy tweaks

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-
-const corsOptions = {
-    origin: "https://www.urbania-custom.com", // Permitir solo este dominio
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true, // Permite cookies si es necesario
-  };
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/api/pins', pinRoutes);
 
 export default app;
