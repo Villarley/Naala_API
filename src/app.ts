511f45a46713f735b6dd73e7b6802a6e3ebfe923
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-22 08:22:48 -0600

    cors to urbania

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import pinRoutes from './routes/pinRoutes';
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(express.json());
 
+const corsOptions = {
+    origin: "https://www.urbania-custom.com", // Permitir solo este dominio
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true, // Permite cookies si es necesario
+  };
+
+app.use(cors(corsOptions));
 app.use('/api/pins', pinRoutes);
 
 export default app;
