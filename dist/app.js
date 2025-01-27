8d4e6f093a03fa05c9e01f0fb43f90c11683ec44
villarley <santivillarley1010@gmail.com>
2025-01-26 23:59:51 -0600

    fix: updatedlinks

--- dist/app.js
+++ dist/app.js
@@ -11,19 +11,20 @@ const docxRoutes_1 = __importDefault(require("./routes/docxRoutes"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 (0, db_1.default)();
-/*
-                                           /generateDocx
-https://naala-api-steel.vercel.app/api/pins/generateDocx
-https://naala-api-steel.vercel.app/api/pins/verifyPin
-*/
 const app = (0, express_1.default)();
 // Use express
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["https://urbania-custom.com/", "https://naala.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: [
+        "https://urbania-custom.com",
+        "https://www.urbania-custom.com",
+        "https://naala.vercel.app",
+    ],
     credentials: true,
 }));
-app.use('/api/pins', pinRoutes_1.default);
-app.use('/api/', docxRoutes_1.default);
+app.use("/", (req, res) => {
+    res.json({ msg: "saludos" });
+});
+app.use("/api/pins", pinRoutes_1.default);
+app.use("/api/docx", docxRoutes_1.default);
 exports.default = app;
