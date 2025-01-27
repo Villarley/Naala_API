e531980e94d516d1fad88bc296ae466d76e0b9dc
villarley <santivillarley1010@gmail.com>
2025-01-26 23:36:52 -0600

    fix: updatedlinks

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import pinRoutes from './routes/pinRoutes';
-import docxRoutes from './routes/docxRoutes';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import pinRoutes from "./routes/pinRoutes";
+import docxRoutes from "./routes/docxRoutes";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
-
-
 
 const app = express();
 // Use express
 app.use(express.json());
 
-app.use(cors({
-    origin: ["https://urbania-custom.com/", "https://naala.vercel.app/"],
+app.use(
+  cors({
+    origin: [
+      "https://urbania-custom.com",
+      "https://www.urbania-custom.com",
+      "https://naala.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  }));
-app.use("/",
-    (req, res)=>{
-        res.json({msg:"saludos"})
-    }
-)
-app.use('/api/pins', pinRoutes);
-app.use('/api/docx', docxRoutes);
+  })
+);
+app.use("/", (req, res) => {
+  res.json({ msg: "saludos" });
+});
+app.use("/api/pins", pinRoutes);
+app.use("/api/docx", docxRoutes);
 
 export default app;
