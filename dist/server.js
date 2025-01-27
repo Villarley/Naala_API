c69105e34315a0c0df5abfb38172c58995f0e022
villarley <santivillarley1010@gmail.com>
2025-01-27 00:09:38 -0600

    fix: updated links

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 5000;
+// it works
 app_1.default.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
